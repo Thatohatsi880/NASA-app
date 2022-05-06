1b8cf6d76f1cee7887257e06c55521a23ea8e9ad
Thatohatsi Matshidiso Tilodi <65616521+Thatohatsi880@users.noreply.github.com>
2022-05-07 00:52:11 +0200

    Add files via upload

---
+++ NASA app.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+import PIL
+from PIL import imageTK, PILImage
+
+GREEN = '#8dc24e'
+
+HEIGHT = 700
+WIDTH = 900
+
+root = tk.Tk()
+root.title('Welcome to my planet app')
