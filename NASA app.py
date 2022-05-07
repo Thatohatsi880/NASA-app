917a990092c8913981b1e62717ea79c9a4f648b2
Thatohatsi Matshidiso Tilodi <65616521+Thatohatsi880@users.noreply.github.com>
2022-05-07 15:11:06 +0200

    Update NASA app.py

--- NASA app.py
+++ NASA app.py
@@ -4,14 +4,27 @@ from tkinter import *
 import PIL
 from PIL import imageTK, PILImage
 
-GREEN = '#8dc24e'
+GRAY = '#808080'
 
 HEIGHT = 700
 WIDTH = 900
 
 root = tk.Tk()
-root.title('Welcome to my planet app')
+root.title('Welcome to my fun planet app')
 
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg='#171717')
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg='#800080')
 canvas.pack()
+
+desc_frame = tk.Frame(root, bg='#0000FF')
+desc_frame.place(relx=0.6, rely=0.1, relheight=0.6, relwidth=0.35)
+
+fact_frame = tk.Frame(root, bg='#0000FF')
+fact_frame.place((relx=0.06, rely=0.72, relheight=0.2, relwidth=0.9)
+                 
+option = ["Uranus", "Neptune", "Jupiter", "Earth", "Stars", "Venus", "Saturn", "Mercury", "Mars", "Pluto"]
+clicked = StringVar()
+clicked.set(option(0))
+drop = OptionMenu(root, clicked, option)
+drop.config(bg ='#0000FF', fg = Gray)
+
 mainloop()
